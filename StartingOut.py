682c15be6a9e2446afe0ecdc9f9822f65a177149
Bigyan Dhakal <bigyandhakal1313@gmail.com>
2025-11-06 18:07:43 -0600

    Initializing

---
+++ StartingOut.py
@@ -0,0 +1,17 @@
+name = "Bigyan Dhakal"
+nikckname = "BRUTAL"    
+age = 21
+height = 5.9      # in feet     
+weight = 70      # in kg
+eyes = "Black"
+teeth = "White"
+hair = "Black"
+print(f"Let's talk about {name}.")
+print(f"He's {height} feet tall.")
+print(f"He's {weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")    
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.") 
